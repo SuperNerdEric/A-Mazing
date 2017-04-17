0ddf979fba9d52fc6246d5fa954619d9f8f37c68
Eric White <eric.white.bastrop@gmail.com>
2017-04-16 20:01:02 -0500

    Added touch even listener to rotate triangle

--- app/src/main/java/com/example/eric/amazing/MainActivity.java
+++ app/src/main/java/com/example/eric/amazing/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.eric.amazing;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +37,47 @@ public class MainActivity extends AppCompatActivity {
 
             // Render the view only when there is a change in the drawing data.
             // To allow the triangle to rotate automatically, this line is commented out:
-            //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        }
+
+        private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+        private float mPreviousX;
+        private float mPreviousY;
+
+        @Override
+        public boolean onTouchEvent(MotionEvent e) {
+            // MotionEvent reports input details from the touch screen
+            // and other input controls. In this case, you are only
+            // interested in events where the touch position changed.
+
+            float x = e.getX();
+            float y = e.getY();
+
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_MOVE:
+
+                    float dx = x - mPreviousX;
+                    float dy = y - mPreviousY;
+
+                    // reverse direction of rotation above the mid-line
+                    if (y > getHeight() / 2) {
+                        dx = dx * -1 ;
+                    }
+
+                    // reverse direction of rotation to left of the mid-line
+                    if (x < getWidth() / 2) {
+                        dy = dy * -1 ;
+                    }
+
+                    mRenderer.setAngle(
+                            mRenderer.getAngle() +
+                                    ((dx + dy) * TOUCH_SCALE_FACTOR));
+                    requestRender();
+            }
+
+            mPreviousX = x;
+            mPreviousY = y;
+            return true;
         }
     }
 }
